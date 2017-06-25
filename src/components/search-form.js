bd9824841de784d9fe5b96bcef50dd9fe2a979e5
Pablo Ledesma <pledesma@fiksu.com>
2017-06-25 11:42:49 -0400

    align elements and create labels

--- src/components/search-form.js
+++ src/components/search-form.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import '../styles/search-form.css';
+
 
 class SearchForm extends Component {
-  debugger;
   render() {
-    return (<h1>Form</h1>);
+    return (
+      <div className="form-group">
+        <div className="location-group">
+          <label>Where</label>
+        </div>
+        <div className="start-date-group">
+          <label>When</label>
+
+        </div>
+        <div className="end-date-group">
+          <label>Until</label>
+        </div>
+        <div className="submit">
+          <i className="fa fa-search" aria-hidden="true"></i>
+        </div>
+      </div>
+    );
   }
 }
 
